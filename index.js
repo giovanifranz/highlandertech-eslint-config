dbb84a692b1929faf9ebbcc6c844c019ebc29088
giovanifranz <giovanifranz151@gmail.com>
2022-08-05 20:47:19 -0300

    [UPDATE ESLINT]

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
+    ecmaVersion: 12,
     sourceType: "module",
   },
   plugins: [
@@ -32,6 +32,17 @@ module.exports = {
     "prettier",
     "simple-import-sort",
   ],
+  settings: {
+    "import/resolver": {
+      typescript: {},
+      react: {
+        version: "detect",
+      },
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
   rules: {
     "prettier/prettier": [
       "error",
@@ -44,7 +55,38 @@ module.exports = {
         singleQuote: true,
       },
     ],
+    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".ts"] }],
     "react/react-in-jsx-scope": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          [
+            "^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)",
+          ],
+          ["^react", "^@?\\w"],
+          ["^(@|@company|@ui|components|utils|config|vendored-lib)(/.*|$)"],
+          ["^\\u0000"],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$", "^.+\\.s?css$"],
+        ],
+      },
+    ],
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "import/prefer-default-export": "off",
     "react/prop-types": "off",
     "jsx-a11y/alt-text": [
       "warn",
@@ -58,18 +100,9 @@ module.exports = {
     "jsx-a11y/aria-unsupported-elements": "warn",
     "jsx-a11y/role-has-required-aria-props": "warn",
     "jsx-a11y/role-supports-aria-props": "warn",
+    "jsx-a11y/anchor-is-valid": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+    "react/require-default-props": "off",
+    "react/jsx-props-no-spreading": "off",
   },
-  settings: {
-    react: {
-      version: "detect",
-    },
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
-    },
-  },
-  "simple-import-sort/imports": "error",
-  "simple-import-sort/exports": "error",
-  "import/first": "error",
-  "import/newline-after-import": "error",
-  "import/no-duplicates": "error",
 };
