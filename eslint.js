cebb4ed6a50efd5dda16fdf8a21652830d6fa1bf
giovanifranz <giovanifranz151@gmail.com>
2022-08-05 23:10:15 -0300

    [UPDATE ESLINT]

--- eslint.js
+++ eslint.js
@@ -39,6 +39,7 @@ module.exports = {
       },
     },
   },
+  ignorePatterns: ["!.*", "dist", "node_modules"],
   rules: {
     "prettier/prettier": ["error", prettierConfig],
     "react/jsx-filename-extension": [1, { extensions: [".tsx", ".ts"] }],
